45983e1e2935368f73cf19ef45207b1badcb7a8e
eldarjahic <eldar.jahic@stu.ibu.edu.ba>
2024-06-03 20:57:41 +0200

    add deployed env to swagger

--- live-backend/rest/routes/ApiSpec.php
+++ live-backend/rest/routes/ApiSpec.php
@@ -4,6 +4,7 @@
 /**
  * @OA\Info(title="Web project", version="0.2", @OA\Contact(email="web", name="Web"))
  * @OA\OpenApi(
+ *    @OA\Server(url="https://orca-app-v9rr8.ondigitalocean.app/live-backend", description="Deployed Environment" ),
  *    @OA\Server(url="http://localhost/noteapp/live-backend", description="Development Environment" ),
  *    @OA\Server(url="https://web-project-priprema.ba/live-backend", description="Production Environment" )
  * ),
